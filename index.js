477d838d6f3e9eff47dd2d3338f5fb7548d2f6eb
Zerojav <javiermero8@gmail.com>
2025-07-02 22:55:24 -0500

    commit

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.json());
 
+// Webhook de Payphone
 app.post('/payphone/webhook', (req, res) => {
   console.log("📩 Webhook recibido:", req.body);
-  res.sendStatus(200); // Payphone necesita que respondas 200 OK
+  res.sendStatus(200);
 });
 
+// Carpeta de archivos públicos (como confirmacion.html)
+app.use(express.static('public'));
+
+// Página de confirmación
+app.get('/confirmacion', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'confirmacion.html'));
+});
+
+// Puerto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor en puerto ${PORT}`);
