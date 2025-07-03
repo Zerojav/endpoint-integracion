27fdf6709ff6404626cbcec804ff64b16c9ce938
Zerojav <javiermero8@gmail.com>
2025-07-02 23:03:48 -0500

    cambios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+const fetch = require('node-fetch'); // Asegúrate de tener node-fetch instalado
 
 app.use(bodyParser.json());
 
@@ -23,4 +24,30 @@ app.get('/confirmacion', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor en puerto ${PORT}`);
+});
+
+
+app.post('/verificar-transaccion', async (req, res) => {
+  const { id, clientTransactionId } = req.body;
+
+  try {
+    const respuesta = await fetch('https://sandbox.payphone.com/api/button/Confirm', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer wrPUSQfjR-GCEFuaIWVGK4wvBycFljhVQTyO8q1i56c7GnoBXh6tosgP5cQxR0Xx5VA4pbb-xb12qVFnkTcR54MR3zx-u16Qf7WKf64We9j-SNXcsCGLICgwiI2bhfurKCtLL2p-XiL8Tx1lxXNou5Tool60cznwFLN57zFBTLgEz5a3nm7wvtakueGM1zqg9Ygs5HuJlmUIY51tfpVgf_gxmAyQvhwi4qfQ8o2XZkmGhlN8bdfM_rpVmjk006JdiehQphBPScuzRHZQYrrnBxfUbZyoaUFrZkicIhtryaDdntIEFuYc8rjHK0vbmZGh7ExJCyBW5EGQiv5QkDCTmhQVXis',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id: parseInt(id),
+        clientTransactionId
+      })
+    });
+
+    const resultado = await respuesta.json();
+    res.json(resultado);
+
+  } catch (error) {
+    console.error("Error consultando la transacción:", error);
+    res.status(500).json({ error: "Fallo al consultar el estado del pago" });
+  }
 });
